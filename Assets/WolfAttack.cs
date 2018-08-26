5f84b0b4ec186ced707f354d031e1b5188c19be2
SamuelQZQ <928221136@qq.com>
2018-08-26 14:31:47 +0800

    Wolf Attack

---
+++ Assets/WolfAttack.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using MalbersAnimations;
+
+public class WolfAttack : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    private void OnCollisionStay(Collision collision)
+    {
+        WolfController wolf = collision.collider.GetComponentInParent<WolfController>();
+        if(wolf.tag == "Wolf")
+        {
+            wolf.Attack1 = true;
+        }
+    }
+
+    private void OnCollisionExit(Collision collision)
+    {
+        WolfController wolf = collision.collider.GetComponentInParent<WolfController>();
+        if (wolf.tag == "Wolf")
+        {
+            wolf.Attack1 = false;
+        }
+    }
+
+}
